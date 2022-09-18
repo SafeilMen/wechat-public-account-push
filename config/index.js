f641f42753bc0b9011197e530eccebde0a6bb07a
niebbo <69680071+niebbo@users.noreply.github.com>
2022-09-18 14:35:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       province: "",
       city: "",
       horoscopeDate: '03-29',
-      horoscopeDateType: '本周',
+      horoscopeDateType: '今日',
       openUrl: "",
       festivals: [],
       customizedDateList: []
